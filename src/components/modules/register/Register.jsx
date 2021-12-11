5cb36065856a463cfa32f774dd9ac2f9a038aee4
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-12-11 18:49:57 -0500

    maquetacion login

--- src/components/modules/register/Register.jsx
+++ src/components/modules/register/Register.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Table, Button, Form, Navbar, FormControl, Container } from "react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../style/style.css';
 
-function Register(props) {
+const Register = () => {
     return (
-       
-    <div>
-          <h1>Desde register</h1>
-    </div>
-        
+        <>
+        <div>
+         <body>
+             <form class="form-register">
+           
+              <h4>Formulario Registro</h4>
+            <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su nombre completo"/>
+            <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese sus apellidos"/>
+            <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su correo"/>
+            <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su contraseña"/>
+            <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
+            <input class="botons" type="submit" value="Registrar"/>
+               <p><a href="#">¿Ya tengo Cuenta?</a></p>
+               </form>
+          </body>
+        </div>
+        </>
     )
 }
 
-Register.propTypes = {
 
-}
 
 export default Register
 
