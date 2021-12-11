53a7980b7fed2513ad1677e4463683860d9fd961
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-12-10 22:07:47 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/App.css';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Sidebar from './components/generics/sidebar/Sidebar';
+
 
 function App() {
   return (
+    <>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Sidebar />   
+
+        </div>
+      
+    </>
   );
 }
 
